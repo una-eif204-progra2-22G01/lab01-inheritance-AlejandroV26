c285c2164dae7b97369b5bb8a52772a65f0b7021
AlejandroV26 <alevargasva@gmail.com>
2022-03-16 09:50:39 -0600

    Implementacion del main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,19 @@
 //
 // Created by Alejandro on 16/03/2022.
 //
+#include "Base.h"
+#include "Derived1.h"
+#include "Derived2.h"
+
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+    cout << "Welcome to the UNA!" << endl << endl;
+
+    Derived1 derived1;
+    derived1.do_something();
+
+    Derived2 derived2;
+    derived2.do_something();
+}
 
